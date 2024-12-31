bc1dbc2f0e86b89fb07e2b44f6e87166f0d0751c
Syed Hani Abidi <syedhani.abidi59@gmail.com>
2025-01-01 01:10:51 +0530

    Adding test cases

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,22 @@ namespace scontainer.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+            //logger = logger;
+        //}
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            ViewData["abc"] = "SomeValue";
+            return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
